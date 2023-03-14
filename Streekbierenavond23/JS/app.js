1207ef8957a742ba90af5ad78fb103388d1f1b3e
JensdbHowest <jens.de.bock@student.howest.be>
2023-03-14 13:40:55 +0100

    Streekbieren 2023 Version 1

---
+++ Streekbierenavond23/JS/app.js
@@ -0,0 +1,57 @@
+let visits;
+let entireVisitPlaceholder;
+let endpointnamespace = "sint-raphael.github.io";
+let endpointkey = "Streekbierenavond23";
+
+
+function liveViews(response) {
+    console.log("This page got " + response.value + " views");
+};
+// The function is called in the CDN on HTML.
+
+
+const getData = (json) => {
+	console.log(json);
+
+	if(json.value != null){
+		console.log("This page got " + json.value + " views")
+		visits.innerText = json.value;
+	}
+	else{
+		console.log("The key used to check the amount of visits is either non existend or expired.")
+		if(entireVisitPlaceholder != null){
+			entireVisitPlaceholder.innerText = "Op dit moment kunnen we het aantal bezoeken niet weergeven. Probeer het later opnieuw.";
+		}
+	}
+};
+
+
+function showVisits() {
+    let endpoint = `https://api.countapi.xyz/info/${endpointnamespace}/${endpointkey}`;
+
+		fetch(endpoint)
+			.then((r) => r.json())
+			.then((json) => {
+				getData(json);
+			})
+			.catch((e) => console.error(e));
+};
+
+
+const init = () => {
+	visits = document.getElementById('visits');
+	entireVisitPlaceholder = document.getElementById('entireVisitPlaceholder');
+	if(visits != null){
+		showVisits();
+		
+		setInterval(() => {
+			showVisits();
+		}, 10000);
+	}
+};
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // console.log("DOM content Loaded");
+	init();
+});
